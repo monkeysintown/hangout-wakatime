6586d5d86902d975c29f7470121cdf18cc288fce
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2015-02-05 18:06:37 +0000

    Dev improvement

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-## Instructions
+## Build
+
+```
+npm install
+```
+
+```
+gulp build
+```
+
+NOTE:
+
+The ZIP file in the dist directory is just for convenience in case multiple artifacts are published on a server. This
+gadget file (wakatime.xml) is self containing (everything is inlined); easier to handle.
+
+## Hangout Integration
 
 ### Dev Console
 
@@ -8,7 +23,12 @@ NOTE:
 
 At the moment it's a bit confusing; the new UI doesn't seem to be complete; some tasks had to be done with old one.
 
-## Install
+### Install
 
 The gadget doesn't have to be public. If you've finished the process with the dev console you'll see it in your Hangout
 widget list (dev category).
+
+### Note
+
+The URL that I am using points to the raw Github file (https://raw.../wakatime.xml). This is just for testing. Ultimately
+it should be hosted on a proper server.
--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,25 @@ var $ = require('gulp-load-plugins')();
 
 var merge = require('merge-stream');
 var pkg = require('./package.json');
-var request = require('request');
+var fs = require('fs');
 
-gulp.task('package', function () {
-    return gulp.src('gadget/**/*')
-        .pipe($.zip(pkg.name + '.zip'))
-        .pipe(gulp.dest('dist'));
+gulp.task('merge', function () {
+    //.pipe($.inject(gulp.src(['./gadget/**/*.js', './gadget/**/*.css'], {read: false})))
+
+    return gulp.src('./gadget/wakatime.xml')
+        .pipe($.replace(/<link href="(.*\.css)"[^>]*>/g, function(s, filename) {
+            var style = fs.readFileSync('./gadget/styles/' + filename, 'utf8');
+            return '<style>\n' + style + '\n</style>';
+        }))
+        .pipe($.replace(/<script src="(wakatime\.js)"[^>]*>/g, function(s, filename) {
+            var script = fs.readFileSync('./gadget/scripts/' + filename, 'utf8');
+            return '<script>\n' + script + '\n</script>';
+        }))
+        .pipe($.replace(/<include src="(.*\.html)"[^>]*>/g, function(s, filename) {
+            var html = fs.readFileSync('./gadget/' + filename, 'utf8');
+            return '\n' + html + '\n';
+        }))
+        .pipe(gulp.dest('public/')); // this should be a temp directory; just adding this to Github to be able to publish directly from the repository
 });
 
 gulp.task('clean', function () {
@@ -22,6 +35,12 @@ gulp.task('clean', function () {
 
     return gulp.src(['.tmp', 'dist'], { read: false })
         .pipe(vinylPaths(del));
+});
+
+gulp.task('build', ['clean', 'merge'], function () {
+    return gulp.src('.tmp/**/*')
+        .pipe($.zip(pkg.name + '.zip'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['package']);
