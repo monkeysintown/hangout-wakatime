37c413538c54fc0edb7e8fc6cab1814e6f5a5c3a
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2015-02-06 02:25:06 +0000

    Whitelist API calls

--- gadget/scripts/wakatime.js
+++ gadget/scripts/wakatime.js
@@ -9,6 +9,14 @@
 
     var app = angular.module('wakatime', []);
 
+    app.config(function ($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+            // Allow same origin resource loads.
+            'self',
+            // Allow loading from our assets domain.  Notice the difference between * and **.
+            API_URL + '/**']);
+    });
+
     app.factory('Hangout', function ($rootScope, $interval, $http, $log) {
         var overlays = {};
         var watch;
